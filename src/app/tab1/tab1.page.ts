f2f366e6b1f14d09ec220d13525457601d59d4f4
lawahit <langelesmuthe@gmail.com>
2023-01-16 19:45:34 -0600

    respaldo1

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+  constructor() {}
+  ngOnInit(): void {
+    console.log("hello luis");
+    let numero =10;
+    debugger;
+    numero=50;
+    numero=0;
+    console.log(numero);
+
+  }
+
+}
